53b4c915a75ad3e8a7d199b1b68d7a9cb5b575e1
Eyup Dogan <eyodohan@gmail.com>
2022-05-08 23:58:22 +0300

    style the header section

--- components/Header.js
+++ components/Header.js
@@ -10,8 +10,8 @@ import {
 
 const Header = () => {
   return (
-    <div>
-      <div className=''>
+    <div className='bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between'>
+      <div className='flex '>
         <HeaderIcon Icon={HomeIcon} title='HOME' />
         <HeaderIcon Icon={UserIcon} title='ACCOUNT' />
         <HeaderIcon Icon={PhoneIcon} title='CONTACT' />
@@ -21,6 +21,7 @@ const Header = () => {
         src='https://wikiimg.tojsiabtv.com/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/1200px-IMDB_Logo_2016.svg.png'
         height={60}
         width={120}
+        className='cursor-pointer active:brightness-110'
       />
     </div>
   );
